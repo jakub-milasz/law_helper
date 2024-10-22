cfba98b1cf568fbc13f312d245271b78b56375d6
Jakub Milasz <jakubmilasz@gmail.com>
2024-10-22 20:11:38 +0200

    Adding connection between pages

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, request, session, flash
+from flask_sqlalchemy import SQLAlchemy
+import pandas
+
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
+  if request.method == 'POST':
+    return redirect(url_for('rules'))
   return render_template('index.html')
 
+@app.route('/rules')
+def rules():
+  return render_template('rules.html')
 if __name__ == '__main__':
   app.run(debug=True)
