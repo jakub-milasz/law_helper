8fc7ed27e1e5830e67ed6f4d2899fb373aeb3c02
Jakub Milasz <jakubmilasz@gmail.com>
2024-10-23 12:56:26 +0200

    Creating database connection

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for, request, session, flash
 from flask_sqlalchemy import SQLAlchemy
-import pandas
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Jakubm123@localhost/Codex'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Criminal(db.Model):
+    __tablename__ = 'criminal'
+    id = db.Column("article_id", db.Integer, primary_key=True)
+    article = db.Column("article_description", db.String(1000))
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -14,6 +21,19 @@ def index():
 
 @app.route('/rules')
 def rules():
-  return render_template('rules.html')
+  rules = Criminal.query.all()
+  return render_template('rules.html', rules=rules)
+
 if __name__ == '__main__':
+  with app.app_context():
+    db.create_all()
   app.run(debug=True)
+
+
+  # try:
+#     conn = psycopg2.connect("postgresql://postgres:Jakubm123@localhost.Codex")
+#     print("Połączono z bazą danych!")
+#     conn.close()
+# except Exception as e:
+#     print(f"Nie udało się połączyć z bazą danych: {e}")
+  
