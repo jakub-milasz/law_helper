fc29324ef5cc7863ba22f37e02e142327672acb6
Jakub Milasz <jakubmilasz@gmail.com>
2024-12-25 21:36:36 +0100

    Adding new formular

--- app.py
+++ app.py
@@ -4,8 +4,14 @@ from dotenv import load_dotenv, find_dotenv
 import google.generativeai as genai
 
 
+# with open("db.csv", "r", encoding="utf-8") as file:
+#   data = file.read()
+
+
+
 prompt_template = """
-Pytanie: Co w Polsce grozi za następujący czyn: {description}. Podaj odpowiedni artykuł i zacytuj go. Sformatuj za pomocą znaczników HTML, to znaczy: Numer artykułu lub artykułów w nagłówku h3, treść artykułów w paragrafach p, w tym zdanie z odpowiedzią wytłuszczone.
+Pytanie: Co w Polsce grzoi za następujący czyn: {description}. Podaj odpowiedni artykuł i przytocz jego treść.
+Sformatuj odpowiedź za pomocą znaczników HTML, to znaczy: Numer artykułu lub artykułów w nagłówku h3, treść artykułów w paragrafach p, w tym zdanie z odpowiedzią wytłuszczone. Jeżeli uznasz opis za nieprecyzyjny, napisz "Doprecyzuj" oraz dopisz, w jaki sposób należy doprecyzować opis. Podaj pytanie pomocnicze w [].
 <h3>Artykuł numer_artykułu</h3>
 <p>...</p>
 """
@@ -14,6 +20,7 @@ def generate_response(description):
   filled_prompt = prompt_template.format(description=description)
   response = model.generate_content(filled_prompt)  
   return response
+
 
 _ = load_dotenv(find_dotenv())
 genai.configure(api_key=os.environ.get("GOOGLE_AI_API_KEY"))
@@ -29,7 +36,6 @@ def index():
   if request.method == 'POST':
     description = request.form['description']
     session['description'] = description
-    # print(dataset['crime'])
     return redirect(url_for('rules'))
   return render_template('index.html')
 
@@ -38,7 +44,21 @@ def rules():
   if "description" in session:
     description = session['description']
     found_article = generate_response(description)
+    i1 = found_article.text.index('[')
+    i2 = found_article.text.index(']')
+    cause = found_article.text[i1+1:i2]
+    session['cause'] = cause
+    if "Doprecyzuj" in found_article.text:
+      return redirect(url_for('additional'))
     return render_template('rules.html', rules=found_article.text)
+  
+@app.route('/additional', methods=['POST', 'GET'])
+def additional():
+  if request.method == 'POST':
+    description = request.form['description']
+    session['description'] = description
+    return redirect(url_for('rules'))
+  return render_template('additional.html', cause = session['cause'])
 
 if __name__ == '__main__':
   app.run(debug=True)
